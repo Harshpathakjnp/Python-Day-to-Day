30de215c24b782128335543867b945b32d7eec8c
Harshpathakjnp <92443560+Harshpathakjnp@users.noreply.github.com>
2022-10-20 22:05:25 +0530

    updated

--- Day-18/main.py
+++ Day-18/main.py
@@ -5,7 +5,7 @@ timmy_the_turtle = Turtle()
 screen = Screen()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("coral3")
-noofsides =50
+noofsides =5
 angle = 360//noofsides
 for i in range(noofsides):
     timmy_the_turtle.forward(10)
