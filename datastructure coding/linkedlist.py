5fee05e592c17d37eda505ff3d577f364de1383e
Harshpathakjnp <92443560+Harshpathakjnp@users.noreply.github.com>
2022-10-23 21:46:51 +0530

    Update linkedlist.py

--- datastructure coding/linkedlist.py
+++ datastructure coding/linkedlist.py
@@ -4,11 +4,54 @@ class Node:
         self.data = data
         self.next = None
 
+
 def printll(head1):
     while head1 != None:
         print(head1.data, end=" -> ")
         head1 = head1.next
     print("None")
+
+
+def length(head):
+    count = 0
+    while head is not None:
+        count += 1
+        head = head.next
+    return count
+
+def insertatIR(head,i,data):
+    if i==0:
+        newNode = Node(data)
+        newNode.next = head
+        return newNode
+    if head is None:
+        return None
+    smallHead = insertatIR(head.next,i-1,data)
+    head.next = smallHead
+    return head
+
+
+def insertatI(head, i, data):
+    if i < 0 or i > length(head):
+        return head
+    count = 0
+    prev = None
+    curr = head
+    while count < i:
+        count += 1
+        prev = curr
+        curr = curr.next
+    newNode = Node(data)
+    if prev is not None:
+        prev.next = newNode
+    else:
+        head = newNode
+        head.next = curr
+    newNode.next = curr
+
+    return head
+
+
 def takeinput():
     inputlist = [int(ele) for ele in input().split()]
     head = None
@@ -29,3 +72,6 @@ def takeinput():
 
 head1 = takeinput()
 printll(head1)
+insertatIR(head1, 2, 9)
+printll(head1)
+printll(insertatIR(head1, 0, 11))
